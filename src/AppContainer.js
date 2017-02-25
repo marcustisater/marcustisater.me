9fcc8f81987d177592877a04b23298acb65a80ad
marcus.tisater@happiness.se <marcus.tisater@happiness.se>
2017-02-25 13:06:19 +0100

    Adds Google Analytics

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -8,20 +8,26 @@ import DefaultHeadMeta from "./components/DefaultHeadMeta"
 import Header from "./components/Header"
 import Content from "./components/Content"
 import Footer from "./components/Footer"
+import GATracker from './components/GoogleAnalyticsTracker'
 
-const AppContainer = (props) => (
-  <Container>
-    <DefaultHeadMeta />
-    <Header />
-    <Content>
-      { props.children }
-    </Content>
-    <Footer />
-  </Container>
-)
+const AppContainer = (props) => {
+  return (
+    <Container>
+      <GATracker params={ props.params }>
+        <DefaultHeadMeta />
+        <Header />
+        <Content>
+          { props.children }
+        </Content>
+        <Footer />
+      </GATracker>
+    </Container>
+  )
+}
 
 AppContainer.propTypes = {
   children: PropTypes.node,
+  params: PropTypes.object
 }
 
 export default AppContainer
